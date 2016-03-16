93dc8931ef1870c3460ec7359d8daed4d0e58132
Pedro Andrade <pedro.andrade@inpe.br>
2016-03-16 10:06:52 -0300

    Tests for save with gal file. #4.

--- test/basic/GPM.lua
+++ test/basic/GPM.lua
@@ -44,5 +44,43 @@ return {
 		unitTest:assertEquals(#gpm.result.relation, #farms)
 		unitTest:assertEquals(#gpm, #farms)
 	end,
+	save = function(unitTest)
+		local roads = CellularSpace{
+			source = filePath("roads.shp", "gpm"),
+			geometry = true
+		}
+
+		local communities = CellularSpace{
+			source = filePath("communities.shp", "gpm"),
+			geometry = true
+		}
+		
+		local farms = CellularSpace{
+			source = filePath("farms.shp", "gpm"),
+			geometry = true
+		}
+
+		local network = Network{
+			lines = roads,
+			destination = communities,
+			weight = function(distance, cell)
+				if cell.CD_PAVIMEN == "pavimentada" then
+					return d / 5
+				else
+					return d / 2
+				end
+			end,
+			outside = function(distance, cell)
+				return distance * 2
+			end
+		}
+
+		local gpm = GPM{
+			network = network,
+			destination = farms
+		}
+
+		gpm:save("farms.gal")
+		unitTest:assertFile("farms.gal")
 }
 
